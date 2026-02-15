4b3af7ee28f20fb212b2841d1373aaebc021c45b
valncabs <142684076+valncabs@users.noreply.github.com>
2026-02-15 17:32:29 -0500

    Refactor JWT handling with FastAPI security
    Updated token expiration time and added HTTPBearer for token verification.

--- FAST-API 1/app/utils/jwt_handler.py
+++ FAST-API 1/app/utils/jwt_handler.py
@@ -1,24 +1,28 @@
-from jose import JWTError, jwt
 from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 SECRET_KEY = "a-string-secret-at-least-256-bits-long"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 2
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-
+security = HTTPBearer()
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
-
-def verify_token(token: str):
+def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTError:
-        return None
+        raise HTTPException(status_code=401, detail="Token inválido o expirado")
 
        
+
