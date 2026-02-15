af5d5453d45ec32bf59d01dfd8f59999e0dae649
valncabs <142684076+valncabs@users.noreply.github.com>
2026-02-15 17:31:35 -0500

    Change password hashing to Argon2 and limit length
    Updated password hashing to use Argon2 and limited password length to 50 characters.

--- FAST-API 1/app/utils/password_handler.py
+++ FAST-API 1/app/utils/password_handler.py
@@ -1,11 +1,10 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password[:50])
 
-def hash_password(password: str):
-    return pwd_context.hash(password)
+def verify_password(password: str, hashed: str) -> bool:
+    return pwd_context.verify(password, hashed)
 
-
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
